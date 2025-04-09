8edebc4046752db715f8c1456f7c360396bdaaf6
rosera <askrichardrose@gmail.com>
2025-04-09 13:46:26 +0200

    Add: Example file to test against

---
+++ development/nix-ruby/test.rb
@@ -0,0 +1,20 @@
+require 'google/cloud/storage'     # Load Google Cloud Storage module
+require 'pp'                       # Load the 'pretty print' module
+
+## BUCKET_NAME = "BUCKET_AVAILABLE"
+BUCKET_NAME = 'BUCKET_UNAVAILABLE'
+
+def gcs_bucket_exists?(bucket_name = BUCKET_NAME)
+  storage = Google::Cloud::Storage.new
+  # bucket = storage.bucket(BUCKET_NAME)
+  bucket = storage.bucket(bucket_name)
+  bucket.nil? ? false : true
+end
+
+if gcs_bucket_exists?
+  puts "Bucket #{BUCKET_NAME} found."
+  # Print a nicely formatted representation of the response
+  ## pp bucket
+else
+    puts "Bucket #{BUCKET_NAME} not found."
+end
