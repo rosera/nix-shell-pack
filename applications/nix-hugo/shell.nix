93575162d259ceafc4de306a69f05d748c5e11ce
rosera <askrichardrose@gmail.com>
2026-02-07 14:52:29 +0000

    Add: Example layout for Hugo blog development

--- applications/nix-hugo/shell.nix
+++ applications/nix-hugo/shell.nix
@@ -1,10 +1,19 @@
 with import <nixpkgs> {};
 
+let
+  # External Script: This reads './dev.kdl' and puts it into a binary named 'dev-zellij'
+  scriptZellijLayout = pkgs.writeText "dev.kdl" (builtins.readFile ./dev.kdl);
+  zellijLayout = pkgs.writeShellScriptBin "dev-zellij" ''
+    ${pkgs.zellij}/bin/zellij --layout ${scriptZellijLayout}
+  '';
+in
 pkgs.mkShell {
   name = "hugo-dev";
 
   nativeBuildInputs = with pkgs; [
     hugo
+    zellij                         # zellij support
+    zellijLayout                   # zellij layout definition
   ];
 
   LANGUAGE     = "Hugo";
@@ -14,5 +23,7 @@ pkgs.mkShell {
     # Optional: Script environment start up 
     echo "Welcome to $LANGUAGE Development Environment"
     $VERSION
+    # Set a layout using Zellij
+    exec dev-zellij
   '';
 }
