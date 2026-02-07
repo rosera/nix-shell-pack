133cefc3219fbe1b6d3ab7f5f50499aacc576ed9
rosera <askrichardrose@gmail.com>
2026-02-07 13:58:22 +0000

    Add: Firebase genkit example

---
+++ applications/nix-firebase-genkit/shell.nix
@@ -0,0 +1,36 @@
+with import <nixpkgs> {};
+
+pkgs.mkShell {
+  name = "firebase-dev";
+
+  nativeBuildInputs = with pkgs; [
+    cacert                         # Support certificates
+    firebase-tools                 # firebase tools
+    git                            # git support
+    nodePackages_latest.webpack    # webpack support
+    nodejs_20                      # nodejs support
+    ollama                         # ollam support
+    vim                            # vim support
+    zellij                         # zellij support
+  ];
+
+  LANGUAGE     = "Firebase Genkit";
+  VERSION      = "firebase --version";
+
+  shellHook = ''
+    # Optional: Script environment start up 
+    echo "Welcome to $LANGUAGE Development Environment"
+    $VERSION
+
+    # Installl packages
+    # npm i --save genkitx-ollama
+    npm i genkitx-ollama
+
+    # Install genkit
+    # npm i -g genkit
+    npm i genkit
+
+    # Set a layout using Zellij
+    zellij --layout layout.kdl
+  '';
+}
